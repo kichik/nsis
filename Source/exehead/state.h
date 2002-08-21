78d1bba90113a2ed640da946c53d50cea439f4fb
Amir Szekely <kichik@users.sourceforge.net>
2002-08-21 19:10:11 +0000

    Language can be choosen from .onInit
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@708 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/exehead/state.h
+++ Source/exehead/state.h
@@ -1,4 +1,4 @@
-extern char g_usrvars[24][NSIS_MAX_STRLEN];
+extern char g_usrvars[25][NSIS_MAX_STRLEN];
 #define state_command_line (g_usrvars[20])
 #define state_install_directory (g_usrvars[21])
 #define state_output_directory (g_usrvars[22])
