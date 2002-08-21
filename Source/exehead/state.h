f51970fd68b207ad3c28def730f7c242e46288cf
Amir Szekely <kichik@users.sourceforge.net>
2002-08-21 19:45:45 +0000

    Fixed a bug that gave EXEDIR, CMDLINE and others wrong values
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@713 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/exehead/state.h
+++ Source/exehead/state.h
@@ -1,8 +1,9 @@
 extern char g_usrvars[25][NSIS_MAX_STRLEN];
-#define state_command_line (g_usrvars[20])
-#define state_install_directory (g_usrvars[21])
-#define state_output_directory (g_usrvars[22])
-#define state_exe_directory (g_usrvars[23])
+#define state_language (g_usrvars[20])
+#define state_command_line (g_usrvars[21])
+#define state_install_directory (g_usrvars[22])
+#define state_output_directory (g_usrvars[23])
+#define state_exe_directory (g_usrvars[24])
 
 extern char g_caption[NSIS_MAX_STRLEN*2];
 extern HWND g_hwnd;
