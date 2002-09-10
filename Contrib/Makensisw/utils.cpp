54425b69700bea88774021ff16fadc9ed20de255
Amir Szekely <kichik@users.sourceforge.net>
2002-09-10 13:50:13 +0000

    Now scrolls on Windows 9x as it should
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@971 212acab6-be3b-0410-9dea-997c60f758d6

--- Contrib/Makensisw/utils.cpp
+++ Contrib/Makensisw/utils.cpp
@@ -60,7 +60,7 @@ extern BOOL g_warnings;
 void LogMessage(HWND hwnd,const char *str) {
 	SendDlgItemMessage(hwnd, IDC_LOGWIN, EM_SETSEL, -1, 0);
 	SendDlgItemMessage(hwnd, IDC_LOGWIN, EM_REPLACESEL, 0, (WPARAM)str);
-	SendDlgItemMessage(hwnd, IDC_LOGWIN, WM_VSCROLL, SB_BOTTOM, 0);
+	SendDlgItemMessage(hwnd, IDC_LOGWIN, EM_SCROLLCARET, /*SB_BOTTOM*/0, 0);
 }
 
 void ErrorMessage(HWND hwnd,const char *str) {
