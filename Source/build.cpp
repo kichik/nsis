f28b32a392a57d040e3fa5f4ce72893fc6769963
Justin Frankel <justin1014@users.sourceforge.net>
2002-09-18 18:39:24 +0000

    Added NSIS_CONFIG_ENHANCEDUI_SUPPORT to config.h
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@1031 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/build.cpp
+++ Source/build.cpp
@@ -1126,7 +1126,9 @@ int CEXEBuild::write_output(void)
       if (resolve_call_int("uninstall callback","un.callbacks",ns_func.find("un.onUninstFailed",0),&build_uninst.common.code_onInstFailed)) return PS_ERROR;
       if (resolve_call_int("uninstall callback","un.callbacks",ns_func.find("un.onUserAbort",0),&build_uninst.common.code_onUserAbort)) return PS_ERROR;
       if (resolve_call_int("uninstall callback","un.callbacks",ns_func.find("un.onNextPage",0),&build_uninst.common.code_onNextPage)) return PS_ERROR;
+    #ifdef NSIS_CONFIG_ENHANCEDUI_SUPPORT
       if (resolve_call_int("uninstall callback","un.callbacks",ns_func.find("un.onInitDialog",0),&build_uninst.common.code_onInitDialog)) return PS_ERROR;
+    #endif
   #endif//NSIS_SUPPORT_CODECALLBACKS
       if (resolve_coderefs("uninstall")) return PS_ERROR;
       set_uninstall_mode(0);
@@ -1148,8 +1150,10 @@ int CEXEBuild::write_output(void)
   if (resolve_call_int("install callback",".callbacks",ns_func.find(".onVerifyInstDir",0),&build_header.code_onVerifyInstDir)) return PS_ERROR;
   if (resolve_call_int("install callback",".callbacks",ns_func.find(".onNextPage",0),&build_header.common.code_onNextPage)) return PS_ERROR;
   if (resolve_call_int("install callback",".callbacks",ns_func.find(".onPrevPage",0),&build_header.code_onPrevPage)) return PS_ERROR;
-  if (resolve_call_int("install callback",".callbacks",ns_func.find(".onInitDialog",0),&build_header.common.code_onInitDialog)) return PS_ERROR;
-  if (resolve_call_int("install callback",".callbacks",ns_func.find(".onMouseOverSection",0),&build_header.code_onMouseOverSection)) return PS_ERROR;
+  #ifdef NSIS_CONFIG_ENHANCEDUI_SUPPORT
+    if (resolve_call_int("install callback",".callbacks",ns_func.find(".onInitDialog",0),&build_header.common.code_onInitDialog)) return PS_ERROR;
+    if (resolve_call_int("install callback",".callbacks",ns_func.find(".onMouseOverSection",0),&build_header.code_onMouseOverSection)) return PS_ERROR;
+  #endif
 #ifdef NSIS_CONFIG_COMPONENTPAGE
   if (resolve_call_int("install callback",".callbacks",ns_func.find(".onSelChange",0),&build_header.code_onSelChange)) return PS_ERROR;
 #endif//NSIS_CONFIG_COMPONENTPAGE
