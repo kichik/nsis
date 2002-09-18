67bbcb08469b5386657cd5453c97f62398d28e4f
Robert Rainwater <rainwater@users.sourceforge.net>
2002-09-18 16:34:39 +0000

    Finished instructions for docs
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@1027 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Docs/src/build.bat
+++ trunk/Docs/src/build.bat
@@ -1,4 +1,4 @@
-bin\halibut.exe config.but intro.but usage.but script.but attributes.but compilerflags.but sections.but functions.but labels.but basic.but registry.but generalpurpose.but flowcontrol.but file.but history.but license.but
+bin\halibut.exe config.but intro.but usage.but script.but attributes.but compilerflags.but sections.but functions.but labels.but basic.but registry.but generalpurpose.but flowcontrol.but file.but misc.but string.but stack.but int.but reboot.but uninstall.but log.but sec.but var.but history.but license.but
 @del *.hlp
 @del *.cnt
 @copy Contents.html index.html
