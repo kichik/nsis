c0f3d19f0e0f160c43bf5cf3a9e8a24529619740
Robert Rainwater <rainwater@users.sourceforge.net>
2002-10-09 17:11:01 +0000

    Added new About dialog
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@1358 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Contrib/Makensisw/resource.h
+++ trunk/Contrib/Makensisw/resource.h
@@ -8,6 +8,7 @@
 #define IDM_MENU                        104
 #define IDK_ACCEL                       105
 #define IDI_SHELL                       112
+#define IDB_LOGO                        115
 #define IDC_LOGWIN                      402
 #define IDC_VERSION                     405
 #define IDC_CLOSE                       406
@@ -24,6 +25,7 @@
 #define IDC_ABOUTPORTIONS               1005
 #define IDC_CONTRIB                     1009
 #define IDC_NSISVER                     1010
+#define IDC_OTHERCONTRIB                1016
 #define IDM_TEST                        40002
 #define IDM_EDITSCRIPT                  40003
 #define IDM_DOCS                        40004
@@ -37,9 +39,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_RESOURCE_VALUE        116
 #define _APS_NEXT_COMMAND_VALUE         40014
-#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
