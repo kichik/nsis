e63f5ed4a9f30ffa515cb1c66e6159bd55b1a661
Robert Rainwater <rainwater@users.sourceforge.net>
2002-10-11 13:38:26 +0000

    Added a Check for Update command
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@1378 212acab6-be3b-0410-9dea-997c60f758d6

--- Contrib/Makensisw/resource.h
+++ Contrib/Makensisw/resource.h
@@ -35,13 +35,14 @@
 #define IDM_CLEARLOG                    40009
 #define IDM_BROWSESCR                   40013
 #define IDM_NSISDEV                     40014
+#define IDM_UPDATE                      40015
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        116
-#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_COMMAND_VALUE         40016
 #define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
