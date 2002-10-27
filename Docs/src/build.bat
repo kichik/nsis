a83ec22a5c0fb9c40188e7989eb5b2c1add6a4a9
Joost Verburg <joostverburg@users.sourceforge.net>
2002-10-27 20:52:52 +0000

    Modern UI appendix
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@1492 212acab6-be3b-0410-9dea-997c60f758d6

--- Docs/src/build.bat
+++ Docs/src/build.bat
@@ -1,4 +1,4 @@
-bin\halibut.exe config.but intro.but usage.but script.but attributes.but compilerflags.but sections.but basic.but registry.but generalpurpose.but flowcontrol.but file.but misc.but string.but stack.but int.but reboot.but uninstall.but log.but sec.but functions.but labels.but var.but usection.but callback.but compiler.but defines.but plugin.but history.but license.but
+bin\halibut.exe config.but intro.but usage.but script.but attributes.but compilerflags.but sections.but basic.but registry.but generalpurpose.but flowcontrol.but file.but misc.but string.but stack.but int.but reboot.but uninstall.but log.but sec.but functions.but labels.but var.but usection.but callback.but compiler.but defines.but plugin.but history.but license.but modernui.but
 @del *.hlp
 @del *.cnt
 @copy Contents.html index.html
