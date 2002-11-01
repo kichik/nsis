55b71b287f510aace22b616c6624bc88b1a67d3c
Robert Rainwater <rainwater@users.sourceforge.net>
2002-11-01 23:01:34 +0000

    last update
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@1552 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Docs/src/bin/halibut/bk_xhtml.c
+++ trunk/Docs/src/bin/halibut/bk_xhtml.c
@@ -1074,17 +1074,17 @@ xhtml_add_contents_entry(FILE * fp, xhtmlsection * section, int limit)
 	fprintf(fp, "<ul>\n");
     }
     fprintf(fp, "<li>");
-    fprintf(fp, "<a href=\"%s#%s\">", section->file->filename,section->fragment);
+    fprintf(fp, "<a %shref=\"%s#%s\">",(section->para->type==para_Chapter||section->para->type==para_Appendix)?"class=\"btitle\" ":"", section->file->filename,section->fragment);
     if (section->para->type==para_Chapter||section->para->type==para_Appendix) fprintf(fp, "<b>");
     if ((section->para->type!=para_Heading&&section->para->type!=para_Subsect)||(section->para->kwtext&&!section->para->words)) {
 	    xhtml_para(fp, section->para->kwtext);
         if (section->para->words)
             fprintf(fp, ": ");
     }
+    if (section->para->type==para_Chapter||section->para->type==para_Appendix) fprintf(fp, "</b>");
     if (section->para->words) {
 	    xhtml_para(fp, section->para->words);
     }
-    if (section->para->type==para_Chapter||section->para->type==para_Appendix) fprintf(fp, "</b>");
     fprintf(fp, "</a></li>\n");
     return TRUE;
 }
