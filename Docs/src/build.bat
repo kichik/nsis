5640f1ad5b1af74fce9b84d032d801a5deeb9615
Robert Rainwater <rainwater@users.sourceforge.net>
2002-11-01 22:04:58 +0000

    no need to delete help files and text files anymore
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@1545 212acab6-be3b-0410-9dea-997c60f758d6

--- Docs/src/build.bat
+++ Docs/src/build.bat
@@ -1,8 +1,5 @@
 bin\halibut.exe config.but intro.but usage.but script.but attributes.but compilerflags.but pages.but sections.but basic.but registry.but generalpurpose.but flowcontrol.but file.but misc.but string.but stack.but int.but reboot.but uninstall.but log.but sec.but functions.but labels.but var.but usection.but callback.but compiler.but defines.but plugin.but history.but modernui.but usefulfunc.but license.but
-@del *.hlp
-@del *.cnt
 @copy Contents.html index.html
 @copy index.html IndexPage.html
-@del output.txt
 @copy *.html ..\\
 @del -f *.html
