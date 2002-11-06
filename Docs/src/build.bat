78ec2f12eac5e3f037f69647ca50e56f2ccce9da
Robert Rainwater <rainwater@users.sourceforge.net>
2002-11-06 21:20:55 +0000

    Updated docs
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@1609 212acab6-be3b-0410-9dea-997c60f758d6

--- Docs/src/build.bat
+++ Docs/src/build.bat
@@ -1,4 +1,4 @@
-bin\halibut.exe config.but intro.but usage.but script.but attributes.but compilerflags.but pages.but sections.but basic.but registry.but generalpurpose.but flowcontrol.but file.but misc.but string.but stack.but int.but reboot.but uninstall.but log.but sec.but functions.but labels.but var.but usection.but callback.but compiler.but defines.but plugin.but history.but modernui.but usefulfunc.but license.but
+bin\halibut.exe config.but intro.but usage.but script.but attributes.but compilerflags.but pages.but sections.but basic.but registry.but generalpurpose.but flowcontrol.but file.but misc.but string.but stack.but int.but reboot.but uninstall.but log.but sec.but ui.but functions.but labels.but var.but usection.but callback.but compiler.but defines.but plugin.but history.but modernui.but usefulfunc.but license.but
 @copy Contents.html index.html
 @copy index.html IndexPage.html
 @copy *.html ..\\
