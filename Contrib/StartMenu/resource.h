b0d8556dbc282430f71063e85b115175e17d3642
Amir Szekely <kichik@users.sourceforge.net>
2002-11-11 17:17:33 +0000

    Added /checknoshortcuts
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@1669 212acab6-be3b-0410-9dea-997c60f758d6

--- Contrib/StartMenu/resource.h
+++ Contrib/StartMenu/resource.h
@@ -7,6 +7,7 @@
 #define IDC_LOCATION                    1002
 #define IDC_TEXT                        1003
 #define IDC_DIRLIST                     1004
+#define IDC_CHECK                       1005
 
 // Next default values for new objects
 // 
@@ -14,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
