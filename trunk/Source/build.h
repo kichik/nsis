c0dda92671227e1aff7113bc05272a0cab47c882
Amir Szekely <kichik@users.sourceforge.net>
2002-12-01 13:19:23 +0000

    Fixed a bug with !if and friends and fixed an ancient bug with line counting going wrong with macros
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@1859 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Source/build.h
+++ trunk/Source/build.h
@@ -83,7 +83,7 @@ class CEXEBuild {
     int parseScript(FILE *fp, const char *curfilename, int *lineptr);
     void ps_addtoline(const char *str, GrowBuf &linedata, StringList &hist);
     int doParse(const char *str, FILE *fp, const char *curfilename, int *lineptr);
-    int doCommand(int which_token, LineParser &line, FILE *fp, const char *curfilename, int linecnt);
+    int doCommand(int which_token, LineParser &line, FILE *fp, const char *curfilename, int *lineptr);
     int do_add_file(const char *lgss, int attrib, int recurse, int linecnt, int *total_files, const char *name_override=0, int generatecode=1, int *data_handle=0);
     GrowBuf m_linebuild; // used for concatenating lines
 
