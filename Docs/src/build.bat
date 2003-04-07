ce9686de0e52dd539d9294d2e45f6887f54bcc1c
Joost Verburg <joostverburg@users.sourceforge.net>
2003-04-07 18:45:28 +0000

    that was test stuff
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@2453 212acab6-be3b-0410-9dea-997c60f758d6

--- Docs/src/build.bat
+++ Docs/src/build.bat
@@ -1,5 +1,4 @@
 bin\halibut.exe config.but intro.but tutorial.but usage.but script.but var.but labels.but jumps.but pages.but sections.but usection.but functions.but callback.but attributes.but compilerflags.but basic.but registry.but generalpurpose.but flowcontrol.but file.but uninstall.but misc.but string.but stack.but int.but reboot.but log.but sec.but ui.but langs.but plugin.but compiler.but defines.but modernui.but usefulfunc.but usefulinfos.but history.but credits.but license.but
-bin\halibut_index.exe config.but intro.but tutorial.but usage.but script.but var.but labels.but jumps.but pages.but sections.but usection.but functions.but callback.but attributes.but compilerflags.but basic.but registry.but generalpurpose.but flowcontrol.but file.but uninstall.but misc.but string.but stack.but int.but reboot.but log.but sec.but ui.but langs.but plugin.but compiler.but defines.but modernui.but usefulfunc.but usefulinfos.but history.but credits.but license.but
 @copy Contents.html index.html
 @copy index.html IndexPage.html
 @copy *.html ..\\
