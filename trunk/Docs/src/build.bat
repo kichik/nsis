78477d5a4a4697e0ce46292b6b6655fd3f1845bd
Amir Szekely <kichik@users.sourceforge.net>
2003-11-03 12:09:24 +0000

    Only copy CHM if it's different
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@3092 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Docs/src/build.bat
+++ trunk/Docs/src/build.bat
@@ -4,7 +4,13 @@ bin\halibut.exe chm_config.but intro.but tutorial.but usage.but script.but var.b
 @copy ..\*.css .
 @hhc nsis.hhp
 @del /F *.html *.hhc *.hhk *.css
-@move nsis.chm ..\..\
+@if not "x%OS%x" == "xWindows_NTx" goto CHMCopy
+@fc /B nsis.chm ..\..\nsis.chm > nul
+@if errorlevel 0 goto SkipCHMCopy
+:CHMCopy
+  @copy nsis.chm ..\..\
+:SkipCHMCopy
+@del nsis.chm
 
 bin\halibut.exe config.but intro.but tutorial.but usage.but script.but var.but labels.but jumps.but pages.but sections.but usection.but functions.but callback.but attributes.but compilerflags.but basic.but registry.but generalpurpose.but flowcontrol.but file.but uninstall.but misc.but string.but stack.but int.but reboot.but log.but sec.but ui.but langs.but plugin.but compiler.but defines.but modernui.but usefulfunc.but usefulinfos.but history.but credits.but license.but
 @echo off
