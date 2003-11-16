a78a838a0c6b5810ed322a721243f3f38a18b0a3
Amir Szekely <kichik@users.sourceforge.net>
2003-11-16 22:38:14 +0000

    Applied patch #838864 - some more optimizations
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@3152 212acab6-be3b-0410-9dea-997c60f758d6

--- Contrib/ExDLL/exdll_with_unit.dpr
+++ Contrib/ExDLL/exdll_with_unit.dpr
@@ -14,7 +14,7 @@ library exdll;
 uses
   nsis, windows;
 
-procedure ex_dll(hwndParent: HWND; string_size: integer; variables: PChar; stacktop: pointer); cdecl;
+procedure ex_dll(const hwndParent: HWND; const string_size: integer; const variables: PChar; const stacktop: pointer); cdecl;
 begin
   // set up global variables
   Init(hwndParent, string_size, variables, stacktop);
@@ -22,7 +22,7 @@ begin
   NSISDialog(GetUserVariable(INST_0), 'The value of $0', MB_OK);
   NSISDialog(PopString, 'pop', MB_OK);
   PushString('Hello, this is a push');
-  SetUserVariable(INST_0, 'This is 0');
+  SetUserVariable(INST_0, 'This is user var $0');
 end;
 
 exports ex_dll;
