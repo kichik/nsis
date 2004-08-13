451ec6855b04c3e18cf950d138a61200e01f202c
Amir Szekely <kichik@users.sourceforge.net>
2004-08-13 20:01:12 +0000

    tabs to spaces
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@3622 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/ResourceVersionInfo.h
+++ Source/ResourceVersionInfo.h
@@ -17,19 +17,19 @@
 #ifndef _WIN32
 // all definitions for non Win32 platforms were taken from MinGW's free Win32 library
 typedef struct tagVS_FIXEDFILEINFO {
-	DWORD dwSignature;
-	DWORD dwStrucVersion;
-	DWORD dwFileVersionMS;
-	DWORD dwFileVersionLS;
-	DWORD dwProductVersionMS;
-	DWORD dwProductVersionLS;
-	DWORD dwFileFlagsMask;
-	DWORD dwFileFlags;
-	DWORD dwFileOS;
-	DWORD dwFileType;
-	DWORD dwFileSubtype;
-	DWORD dwFileDateMS;
-	DWORD dwFileDateLS;
+  DWORD dwSignature;
+  DWORD dwStrucVersion;
+  DWORD dwFileVersionMS;
+  DWORD dwFileVersionLS;
+  DWORD dwProductVersionMS;
+  DWORD dwProductVersionLS;
+  DWORD dwFileFlagsMask;
+  DWORD dwFileFlags;
+  DWORD dwFileOS;
+  DWORD dwFileType;
+  DWORD dwFileSubtype;
+  DWORD dwFileDateMS;
+  DWORD dwFileDateLS;
 } VS_FIXEDFILEINFO;
 #endif
 
