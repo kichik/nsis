5fc7a3bba1975b7f3d32d2e1fecd31a1d37b194a
Amir Szekely <kichik@users.sourceforge.net>
2005-04-19 18:10:45 +0000

    fixed compile error without NSIS_CONFIG_COMPONENTPAGE
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@3971 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/exehead/components.h
+++ Source/exehead/components.h
@@ -3,7 +3,9 @@
 
 void NSISCALL SectionFlagsChanged(unsigned int index);
 void NSISCALL RefreshSectionGroups();
+#ifdef NSIS_CONFIG_COMPONENTPAGE
 void NSISCALL SetInstType(int inst_type);
 unsigned int NSISCALL GetInstType(HTREEITEM *items);
+#endif//NSIS_CONFIG_COMPONENTPAGE
 
 #endif//!___COMPONENTS_H___
