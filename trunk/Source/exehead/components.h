c6e8865653e2ffaf2ddf581ca376e1a5876c1486
Amir Szekely <kichik@users.sourceforge.net>
2005-09-09 14:29:12 +0000

    size optimization
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@4253 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Source/exehead/components.h
+++ trunk/Source/exehead/components.h
@@ -2,7 +2,8 @@
 #define ___COMPONENTS_H___
 
 void NSISCALL SectionFlagsChanged(unsigned int index);
-void NSISCALL RefreshSectionGroups();
+#define RefreshSectionGroups() _RefreshSectionGroups(0, 0)
+unsigned int NSISCALL _RefreshSectionGroups(unsigned int i, int not_first_call);
 #ifdef NSIS_CONFIG_COMPONENTPAGE
 void NSISCALL SetInstType(int inst_type);
 unsigned int NSISCALL GetInstType(HTREEITEM *items);
