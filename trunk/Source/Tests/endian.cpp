8e6905fdeb131efd7e6d4ddf7d6a02636d768099
Amir Szekely <kichik@users.sourceforge.net>
2006-03-11 11:16:18 +0000

    tests for 16-bit endian conversion
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@4570 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Source/Tests/endian.cpp
+++ trunk/Source/Tests/endian.cpp
@@ -5,7 +5,8 @@ class EndianTest : public CppUnit::TestFixture {
 
   CPPUNIT_TEST_SUITE( EndianTest );
   CPPUNIT_TEST( testSwapEndian );
-  CPPUNIT_TEST( testFixEndian );
+  CPPUNIT_TEST( testFixEndian16 );
+  CPPUNIT_TEST( testFixEndian32 );
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -13,9 +14,12 @@ public:
     CPPUNIT_ASSERT_EQUAL( (int)0x78563412, (int)SWAP_ENDIAN_INT32(0x12345678) );
     CPPUNIT_ASSERT_EQUAL( (int)0xFFFFFFFF, (int)SWAP_ENDIAN_INT32(0xFFFFFFFF) );
     CPPUNIT_ASSERT_EQUAL( (int)0, (int)SWAP_ENDIAN_INT32(0) );
+    CPPUNIT_ASSERT_EQUAL( (int)0x3412, (int)SWAP_ENDIAN_INT16(0x1234) );
+    CPPUNIT_ASSERT_EQUAL( (int)0xFFFF, (int)SWAP_ENDIAN_INT16(0xFFFF) );
+    CPPUNIT_ASSERT_EQUAL( (int)0, (int)SWAP_ENDIAN_INT16(0) );
   }
 
-  void testFixEndian() {
+  void testFixEndian32() {
     int i=1;
     int actual = 0x12345678;
     FIX_ENDIAN_INT32_INPLACE(actual);
@@ -27,6 +31,22 @@ public:
     else {
       // big endian
       expected = 0x78563412;
+    }
+    CPPUNIT_ASSERT_EQUAL(expected, actual);
+  }
+
+  void testFixEndian16() {
+    int i=1;
+    int actual = 0x1234;
+    FIX_ENDIAN_INT16_INPLACE(actual);
+    int expected;
+    if (((char*)&i)[0] == 1) {
+      // little endian
+      expected = 0x1234;
+    }
+    else {
+      // big endian
+      expected = 0x3412;
     }
     CPPUNIT_ASSERT_EQUAL(expected, actual);
   }
