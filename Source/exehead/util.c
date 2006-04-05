069e574eba065434eaa421a28ea3822484b3a22b
Amir Szekely <kichik@users.sourceforge.net>
2006-04-05 18:45:03 +0000

    implemented RFE #1460586 - Put time stamps in the install log
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@4635 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/exehead/util.c
+++ Source/exehead/util.c
@@ -730,7 +730,7 @@ void NSISCALL validate_filename(char *in) {
 
 #ifdef NSIS_CONFIG_LOG
 int log_dolog;
-char log_text[NSIS_MAX_STRLEN*4];
+char log_text[2048]; // 1024 for each wsprintf
 
 #if !defined(NSIS_CONFIG_LOG_ODS) && !defined(NSIS_CONFIG_LOG_STDOUT)
 void NSISCALL log_write(int close)
@@ -810,11 +810,26 @@ void _LogData2Hex(char *buf, size_t buflen, unsigned char *data, size_t datalen)
     mystrcat(buf, "...");
 }
 
+#ifdef NSIS_CONFIG_LOG_TIMESTAMP
+void log_timestamp(char *buf)
+{
+  SYSTEMTIME st;
+  GetLocalTime(&st);
+  wsprintf(buf,"[%04hu/%02hu/%02hu %02hu:%02hu:%02hu] ", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+}
+#else
+#  define log_timestamp(x)
+#endif//NSIS_CONFIG_LOG_TIMESTAMP
+
 void log_printf(char *format, ...)
 {
   va_list val;
   va_start(val,format);
-  wvsprintf(log_text,format,val);
+
+  log_text[0] = '\0';
+  log_timestamp(log_text);
+  wvsprintf(log_text+mystrlen(log_text),format,val);
+
   va_end(val);
 #ifdef NSIS_CONFIG_LOG_ODS
   if (log_dolog)
