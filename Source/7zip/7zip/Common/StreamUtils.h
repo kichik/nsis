ff900b1c4b7eef93f8815c536a700dafa403992c
Joost Verburg <joostverburg@users.sourceforge.net>
2006-10-28 19:45:02 +0000

    * The LZMA compression module for NSIS is now licensed under the Common Public License version 1.0 * Added license and copyright notice to every source file * The new COPYING file and license section in the Users Manual list the terms of all relevant licenses
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@4787 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/7zip/7zip/Common/StreamUtils.h
+++ Source/7zip/7zip/Common/StreamUtils.h
@@ -1,4 +1,19 @@
-// StreamUtils.h
+/*
+ * StreamUtils.h
+ * 
+ * This file is a part of LZMA compression module for NSIS.
+ * 
+ * Original LZMA SDK Copyright (C) 1999-2006 Igor Pavlov
+ * Modifications Copyright (C) 2003-2006 Amir Szekely <kichik@netvision.net.il>
+ * 
+ * Licensed under the Common Public License version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ * Licence details can be found in the file COPYING.
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty.
+ */
 
 #ifndef __STREAMUTILS_H
 #define __STREAMUTILS_H
--- Source/exehead/lang.h
+++ Source/exehead/lang.h
@@ -1,3 +1,19 @@
+/*
+ * lang.c
+ * 
+ * This file is a part of NSIS.
+ * 
+ * Copyright (C) 1999-2006 Nullsoft and Contributors
+ * 
+ * Licensed under the zlib/libpng license (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ * Licence details can be found in the file COPYING.
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty.
+ */
+
 #ifndef _NSIS_LANG_H_
 #define _NSIS_LANG_H_
 
