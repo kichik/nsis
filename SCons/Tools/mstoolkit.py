1af26accb64955e905bd45bfe5c195e7f03815fc
Amir Szekely <kichik@users.sourceforge.net>
2006-12-06 06:15:40 +0000

    spaces to tabs
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@4837 212acab6-be3b-0410-9dea-997c60f758d6

--- SCons/Tools/mstoolkit.py
+++ SCons/Tools/mstoolkit.py
@@ -338,7 +338,7 @@ def generate(env):
 	env['REGSVRFLAGS'] = '/s '
 	env['REGSVRCOM'] = '$REGSVR $REGSVRFLAGS $TARGET'
 
-  env['MSVS_VERSION'] = '7.1'
+	env['MSVS_VERSION'] = '7.1'
 
 
 def exists(env):
