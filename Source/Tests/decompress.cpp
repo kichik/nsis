37a799c2fb7caef7ef6a294e319a67e3376dfec8
Amir Szekely <kichik@users.sourceforge.net>
2007-01-23 16:05:03 +0000

    compression tests
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@4862 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ Source/Tests/decompress.cpp
@@ -0,0 +1,59 @@
+#include "decompress.h"
+
+#define EXEHEAD
+#define NSIS_CONFIG_COMPRESSION_SUPPORT
+
+extern "C" {
+#define NSIS_COMPRESS_USE_BZIP2
+#include "../bzip2/bzlib.h"
+#undef NSIS_COMPRESS_USE_BZIP2
+
+#define NSIS_COMPRESS_USE_LZMA
+#include "../7zip/LZMADecode.h"
+#undef NSIS_COMPRESS_USE_LZMA
+
+#define NSIS_COMPRESS_USE_ZLIB
+#include "../zlib/ZLIB.H"
+#undef NSIS_COMPRESS_USE_ZLIB
+}
+
+#define DECOMPRESSOR(name, type, initf, dec, u)   \
+  name::name() {                                  \
+    vs = new type;                                \
+  }                                               \
+                                                  \
+  name::~name() {                                 \
+    delete vs;                                    \
+    vs = NULL;                                    \
+  }                                               \
+                                                  \
+  void name::setNextIn(void *buffer, int size) {  \
+    type *s = (type *) vs;                        \
+    s->next_in = (u *) buffer;                    \
+    s->avail_in = size;                           \
+  }                                               \
+                                                  \
+  void name::setNextOut(void *buffer, int size) { \
+    type *s = (type *) vs;                        \
+    s->next_out = (u *) buffer;                   \
+    s->avail_out = size;                          \
+  }                                               \
+                                                  \
+  int name::getAvailOut() {                       \
+    type *s = (type *) vs;                        \
+    return s->avail_out;                          \
+  }                                               \
+                                                  \
+  void name::init() {                             \
+    type *s = (type *) vs;                        \
+    initf(s);                                     \
+  }                                               \
+                                                  \
+  int name::decompress() {                        \
+    type *s = (type *) vs;                        \
+    return dec(s);                                \
+  }
+
+DECOMPRESSOR(lzmaDecompressor, lzma_stream, lzmaInit, lzmaDecode, unsigned char);
+DECOMPRESSOR(bzip2Decompressor, DState, BZ2_bzDecompressInit, BZ2_bzDecompress, char);
+DECOMPRESSOR(zlibDecompressor, z_stream, inflateReset, inflate, unsigned char);
