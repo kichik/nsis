80bfbb9be7be36c90bb4edd3513c02e586751486
Amir Szekely <kichik@users.sourceforge.net>
2007-01-23 16:05:03 +0000

    compression tests
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@4862 212acab6-be3b-0410-9dea-997c60f758d6

---
+++ Source/Tests/memcpy.c
@@ -0,0 +1,11 @@
+#include "../exehead/util.h"
+
+void NSISCALL mini_memcpy(void *out, const void *in, int len)
+{
+  char *c_out=(char*)out;
+  char *c_in=(char *)in;
+  while (len-- > 0)
+  {
+    *c_out++=*c_in++;
+  }
+}
