60e9b0fce97b843078fa24b262f077bb04b6ca31
Amir Szekely <kichik@users.sourceforge.net>
2007-01-25 13:04:52 +0000

    add support for codepage selection in winchar_*ansi
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@4886 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Source/winchar.h
+++ trunk/Source/winchar.h
@@ -1,7 +1,7 @@
 #include "Platform.h"
 
-WCHAR *winchar_fromansi(const char* s);
-char *winchar_toansi(const WCHAR* ws);
+WCHAR *winchar_fromansi(const char* s, unsigned int codepage = CP_ACP);
+char *winchar_toansi(const WCHAR* ws, unsigned int codepage = CP_ACP);
 WCHAR *winchar_strcpy(WCHAR *ws1, const WCHAR *ws2);
 WCHAR *winchar_strncpy(WCHAR *ws1, const WCHAR *ws2, size_t n);
 size_t winchar_strlen(WCHAR *ws);
