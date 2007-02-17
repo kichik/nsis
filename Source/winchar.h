5f9ccbff541b70910e5fc5e8b2d06ce902e81dfa
Amir Szekely <kichik@users.sourceforge.net>
2007-02-17 15:06:53 +0000

    added winchar_strdup
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@4936 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/winchar.h
+++ Source/winchar.h
@@ -5,5 +5,6 @@ char *winchar_toansi(const WCHAR* ws, unsigned int codepage = CP_ACP);
 WCHAR *winchar_strcpy(WCHAR *ws1, const WCHAR *ws2);
 WCHAR *winchar_strncpy(WCHAR *ws1, const WCHAR *ws2, size_t n);
 size_t winchar_strlen(WCHAR *ws);
+WCHAR *winchar_strdup(WCHAR *ws);
 int winchar_strcmp(const WCHAR *ws1, const WCHAR *ws2);
 int winchar_stoi(const WCHAR *ws);
