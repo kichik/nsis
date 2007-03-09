e21c82f084f103bd244b83b989c8d0b274f440cb
Amir Szekely <kichik@users.sourceforge.net>
2007-03-09 18:11:53 +0000

    applied patch #1677482 - wxbuild.bat for paths with spaces
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@4985 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Contrib/NSIS Menu/wx/wxbuild.bat
+++ trunk/Contrib/NSIS Menu/wx/wxbuild.bat
@@ -1,13 +1,13 @@
-if not exist %WXWIN%\build\msw\makefile.vc goto error
+if not exist "%WXWIN%\build\msw\makefile.vc" goto error
 set OLDCD=%CD%
-cd %WXWIN%
-copy /y %WXWIN%\include\wx\msw\setup.h %OLDCD%\old_setup.h
-copy /y %OLDCD%\setup.h %WXWIN%\include\wx\msw\setup.h
-rd /S ..\..\lib\vc_libnsis
-rd /S vc_mswnsis
+cd "%WXWIN%"
+copy /y "%WXWIN%\include\wx\msw\setup.h" "%OLDCD%\old_setup.h"
+copy /y "%OLDCD%\setup.h" "%WXWIN%\include\wx\msw\setup.h"
+rd /S /Q ..\..\lib\vc_libnsis
+rd /S /Q vc_mswnsis
 nmake -f makefile.vc CFG=nsis BUILD=release RUNTIME_LIBS=dynamic SHARED=0 UNICODE=0 WXUNIV=0 USE_OPENGL=0 USE_ODBC=0 USE_HTML=1 USE_XRC=0 
-copy /y %OLDCD%\old_setup.h %WXWIN%\include\wx\msw\setup.h
-cd %OLDCD%
+copy /y "%OLDCD%\old_setup.h" "%WXWIN%\include\wx\msw\setup.h"
+cd "%OLDCD%"
 goto done
 :error
 echo WXWIN is not properly set
