e61c8683d309c657bfcc1c8f18cc95d7ea1470b3
Amir Szekely <kichik@users.sourceforge.net>
2007-04-26 18:24:35 +0000

    fixed bug #1707823 - VS_FIXEDFILEINFO undefind in util.cpp
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@5124 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/Platform.h
+++ Source/Platform.h
@@ -849,6 +849,21 @@ typedef struct _IMAGE_EXPORT_DIRECTORY {
   DWORD AddressOfNames;
   DWORD AddressOfNameOrdinals;
 } IMAGE_EXPORT_DIRECTORY,*PIMAGE_EXPORT_DIRECTORY;
+typedef struct tagVS_FIXEDFILEINFO {
+  DWORD dwSignature;
+  DWORD dwStrucVersion;
+  DWORD dwFileVersionMS;
+  DWORD dwFileVersionLS;
+  DWORD dwProductVersionMS;
+  DWORD dwProductVersionLS;
+  DWORD dwFileFlagsMask;
+  DWORD dwFileFlags;
+  DWORD dwFileOS;
+  DWORD dwFileType;
+  DWORD dwFileSubtype;
+  DWORD dwFileDateMS;
+  DWORD dwFileDateLS;
+} VS_FIXEDFILEINFO;
 #  pragma pack()
 #endif
 
