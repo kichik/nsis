e5743ce825f5fa96c91cecfdb0865e98a815f951
Amir Szekely <kichik@users.sourceforge.net>
2008-01-26 11:47:01 +0000

    zero icon memory to prevent uninitialized memory from getting into the resources
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@5492 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/icon.cpp
+++ Source/icon.cpp
@@ -259,6 +259,7 @@ void set_icon(CResourceEditor* re, WORD wIconId, IconGroup icon1, IconGroup icon
   for (order_index = 0; order_index < order.size(); order_index++)
   {
     LPBYTE data = new BYTE[order[order_index].size];
+    memset(data, 0, order[order_index].size);
 
     if (order_index < icon1.size())
     {
