074c98f53fd464401d5b0eb31651acde655fab94
Amir Szekely <kichik@users.sourceforge.net>
2008-08-16 20:26:48 +0000

    need to close the file so it can be deleted
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@5711 212acab6-be3b-0410-9dea-997c60f758d6

--- Scripts/release.py
+++ Scripts/release.py
@@ -373,6 +373,7 @@ def UploadFiles():
 	sftpcmds.write('put %s\\nsis-%s.zip\n' % (newverdir, VERSION))
 	sftpcmds.write('put nsis-%s-log.zip\n' % VERSION)
 	sftpcmds.write('put nsis-%s-strlen_8192.zip\n' % VERSION)
+	sftpcmds.close()
 
 	run(
 		SFTP % 'sftp-commands',
