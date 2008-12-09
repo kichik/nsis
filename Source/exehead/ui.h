f04a1101c06c29992943a54f67865e5d214ce011
Amir Szekely <kichik@users.sourceforge.net>
2008-12-09 22:53:39 +0000

    move definitions common to both plug-ins and stubs to api.h (part of patch #2359978)
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@5815 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/exehead/ui.h
+++ Source/exehead/ui.h
@@ -36,17 +36,11 @@ void NSISCALL build_g_logfile(void);
 // sent to every child window to tell it it can start executing NSIS code
 #define WM_NOTIFY_START (WM_USER+0x5)
 
-// sent to the outer window to tell it to go to the next inner window
-#define WM_NOTIFY_OUTER_NEXT (WM_USER+0x8)
-
 // sent to every child window to tell it it is closing soon
 #define WM_NOTIFY_INIGO_MONTOYA (WM_USER+0xb)
 
 // update message used by DirProc and SelProc for space display
 #define WM_IN_UPDATEMSG (WM_USER+0xf)
-
-// custom pages should send this message to let NSIS know they're ready
-#define WM_NOTIFY_CUSTOM_READY (WM_USER+0xd)
 
 // simulates clicking on the tree
 #define WM_TREEVIEW_KEYHACK (WM_USER+0x13)
