dcde25d6d1e5f454462470ffdaf4f7be3cf9b9cd
Amir Szekely <kichik@users.sourceforge.net>
2008-12-09 22:20:59 +0000

    add standard license headers
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@5813 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Source/exehead/plugin.h
+++ trunk/Source/exehead/plugin.h
@@ -1,5 +1,21 @@
-#ifndef _PLUGINCALLBACKS_H_
-#define _PLUGINCALLBACKS_H_
+/*
+ * plugin.h
+ * 
+ * This file is a part of NSIS.
+ * 
+ * Copyright (C) 1999-2008 Nullsoft and Contributors
+ * 
+ * Licensed under the zlib/libpng license (the "License");
+ * you may not use this file except in compliance with the License.
+ * 
+ * Licence details can be found in the file COPYING.
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty.
+ */
+
+#ifndef _PLUGIN_H_
+#define _PLUGIN_H_
 
 #include "../Platform.h"
 #include "fileform.h"
@@ -33,4 +49,4 @@ extern BOOL NSISCALL Plugins_CanUnload(HANDLE pluginHandle);
 
 #endif /* #ifdef NSIS_CONFIG_PLUGIN_SUPPORT */
 
-#endif /* _PLUGINCALLBACKS_H_ */
+#endif /* _PLUGIN_H_ */
