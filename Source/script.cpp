23c7fe0378e74b33acf7dbfb1748d83abc1f2b5e
Amir Szekely <kichik@users.sourceforge.net>
2009-06-06 13:59:38 +0000

    fixed bug #2796189 - ExecShell doesn't always respect maximized window setting
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@5970 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/script.cpp
+++ Source/script.cpp
@@ -3888,8 +3888,8 @@ int CEXEBuild::doCommand(int which_token, LineParser &line)
       ent.offsets[3]=SW_SHOWNORMAL;
       if (line.getnumtokens() > 4)
       {
-        int tab[4]={SW_SHOWNORMAL,SW_SHOWMAXIMIZED,SW_SHOWMINIMIZED,SW_HIDE};
-        int a=line.gettoken_enum(4,"SW_SHOWNORMAL\0SW_SHOWMAXIMIZED\0SW_SHOWMINIMIZED\0SW_HIDE\0");
+        int tab[5]={SW_SHOWDEFAULT,SW_SHOWNORMAL,SW_SHOWMAXIMIZED,SW_SHOWMINIMIZED,SW_HIDE};
+        int a=line.gettoken_enum(4,"SW_SHOWDEFAULT\0SW_SHOWNORMAL\0SW_SHOWMAXIMIZED\0SW_SHOWMINIMIZED\0SW_HIDE\0");
         if (a < 0) PRINTHELP()
         ent.offsets[3]=tab[a];
       }
