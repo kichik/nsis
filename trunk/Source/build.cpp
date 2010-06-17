bc98aac2e2f3675611ee5e6d066092eaddc8cd29
Wizou <wizou@users.sourceforge.net>
2010-06-17 15:56:42 +0000

    Improving compression performance (avoiding unnecessary file flush and better handling the 2GB limit)
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS@6109 212acab6-be3b-0410-9dea-997c60f758d6

--- trunk/Source/build.cpp
+++ trunk/Source/build.cpp
@@ -794,9 +794,8 @@ int CEXEBuild::add_db_data(IMMap *mmap) // returns offset
   {
     // grow datablock so that there is room to compress into
     int bufferlen = length + 1024 + length / 4; // give a nice 25% extra space
-    if (bufferlen < 0) // too much data... try allocating as much as possible
-      db->resize(max(st, 0x7fffffff));
-    else
+    if (st+bufferlen+sizeof(int) < 0)          // we've hit a signed integer overflow (file is over 1.6 GB)
+        bufferlen = INT_MAX-st-sizeof(int); //  so maximize compressor room and hope the file compresses well
       db->resize(st + bufferlen + sizeof(int));
 
     int n = compressor->Init(build_compress_level, build_compress_dict_size);
