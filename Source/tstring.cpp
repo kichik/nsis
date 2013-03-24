753100678048d39d3364c4c55762ee9ce3b5433e
Amir Szekely <kichik@users.sourceforge.net>
2013-03-24 06:08:16 +0000

    MultiByteToWideChar is defined by tchar.h, but tchar.h includes tstring.h also, it seems _TSTRING_H_ is used by something else
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@6313 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/tstring.cpp
+++ Source/tstring.cpp
@@ -101,4 +101,50 @@ FILE* FileOpenUnicodeText(const TCHAR* file, const TCHAR* mode, BOOL* unicode)
 	return _tfopen(file, strMode.c_str());
 }
 
+CtoTString::CtoTString(const char* str)
+{
+	int len = MultiByteToWideChar(CP_ACP, 0, str, -1, NULL, 0);
+	m_wStr = (wchar_t*) malloc(len*sizeof(wchar_t));
+	MultiByteToWideChar(CP_ACP, 0, str, -1, m_wStr, len);
+}
+
+CtoTString::CtoTString(const char* str, UINT cp)
+{
+	int len = MultiByteToWideChar(cp, 0, str, -1, NULL, 0);
+	m_wStr = (wchar_t*) malloc(len*sizeof(wchar_t));
+	MultiByteToWideChar(cp, 0, str, -1, m_wStr, len);
+}
+
+CtoTString::CtoTString(const std::string& str)
+{
+	int len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length()+1, NULL, 0);
+	m_wStr = (wchar_t*) malloc(len*sizeof(wchar_t));
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), str.length()+1, m_wStr, len);
+}
+
+CtoTString::~CtoTString() { free(m_wStr); m_wStr = 0; }
+
+operator const CtoTString::wchar_t*() const { return GetTStr(); }
+inline const wchar_t* CtoTString::GetTStr() const { return m_wStr; }
+
+
+
+TtoCString::TtoCString(const wchar_t* wStr)
+{
+	int len = WideCharToMultiByte(CP_ACP, 0, wStr, -1, NULL, 0, 0, 0);
+	m_cStr = (char*) malloc(len);
+	WideCharToMultiByte(CP_ACP, 0, wStr, -1, m_cStr, len, 0, 0);
+}
+
+TtoCString::TtoCString(const tstring& wStr)
+{
+	int len = WideCharToMultiByte(CP_ACP, 0, wStr.c_str(), wStr.length()+1, NULL, 0, 0, 0);
+	m_cStr = (char*) malloc(len);
+	WideCharToMultiByte(CP_ACP, 0, wStr.c_str(), wStr.length()+1, m_cStr, len, 0, 0);
+}
+
+TtoCString::~TtoCString() { free(m_cStr); m_cStr = 0; }
+
+operator const TtoCString::char*() const { return m_cStr; }
+
 #endif
