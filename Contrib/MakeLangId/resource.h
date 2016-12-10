132ccf8aafca8e20e432f34427635ba67984c5ec
Anders <anders_k@users.sourceforge.net>
2016-12-10 01:52:59 +0000

    MakeLangId rewritten from scratch to support EnumSystemLocales and our internal list
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@6815 212acab6-be3b-0410-9dea-997c60f758d6

--- Contrib/MakeLangId/resource.h
+++ Contrib/MakeLangId/resource.h
@@ -4,18 +4,22 @@
 //
 #define IDR_MANIFEST                    1
 #define IDD_DIALOG                      101
-#define IDC_SUB                         1001
-#define IDC_PRIMARY                     1002
-#define IDC_RESULT                      1003
-#define IDC_CODEPAGE                    1004
+#define IDI_ICON                        102
+#define IDC_SOURCE                      1001
+#define IDC_SYSLANGLIST                 1002
+#define IDC_INTLANGLIST                 1003
+#define IDC_INFO                        1004
+#define IDC_LANGID                      1005
+#define IDC_CODEPAGE                    1006
+#define IDC_COPYHELPER                  1007
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
