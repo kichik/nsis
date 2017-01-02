295de1a230bc91478d5f9641d5c5735bfdd26244
Anders <anders_k@users.sourceforge.net>
2017-01-02 03:51:39 +0000

    (C) 2017
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@6834 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/validateunicode.h
+++ Source/validateunicode.h
@@ -2,7 +2,7 @@
 //
 // This file is a part of Unicode NSIS.
 //
-// Copyright (C) 2009-2016 Jim Park
+// Copyright (C) 2009-2017 Jim Park
 //
 // Licensed under the zlib/libpng license (the "License");
 // you may not use this file except in compliance with the License.
