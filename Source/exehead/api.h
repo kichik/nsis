92176b08ab2b4f357035fda5096939434439f5d2
Anders <anders_k@users.sourceforge.net>
2017-06-23 12:59:44 +0000

    Logging build now prints EW_SETFLAG changes
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@6876 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/exehead/api.h
+++ Source/exehead/api.h
@@ -40,20 +40,20 @@ typedef UINT_PTR (*NSISPLUGINCALLBACK)(enum NSPIM);
 // besides the stack, variables and HWND passed on to plug-ins.
 typedef struct
 {
-  int autoclose;
-  int all_user_var; // SetShellVarContext: User context = 0, Machine context = 1
-  int exec_error; // IfErrors
-  int abort; // IfAbort
-  int exec_reboot; // NSIS_SUPPORT_REBOOT
-  int reboot_called; // NSIS_SUPPORT_REBOOT
-  int XXX_cur_insttype; // Deprecated
-  int plugin_api_version; // See NSISPIAPIVER_CURR (Note: used to be XXX_insttype_changed)
-  int silent; // NSIS_CONFIG_SILENT_SUPPORT
-  int instdir_error;
-  int rtl;
-  int errlvl; // SetErrorLevel
-  int alter_reg_view; // SetRegView: Default View = 0, Alternative View = (sizeof(void*) > 4 ? KEY_WOW64_32KEY : KEY_WOW64_64KEY)
-  int status_update; // SetDetailsPrint
+  int autoclose;          // SetAutoClose
+  int all_user_var;       // SetShellVarContext: User context = 0, Machine context = 1
+  int exec_error;         // IfErrors
+  int abort;              // IfAbort
+  int exec_reboot;        // IfRebootFlag (NSIS_SUPPORT_REBOOT)
+  int reboot_called;      // NSIS_SUPPORT_REBOOT
+  int XXX_cur_insttype;   // Deprecated
+  int plugin_api_version; // Plug-in ABI. See NSISPIAPIVER_CURR (Note: used to be XXX_insttype_changed)
+  int silent;             // IfSilent (NSIS_CONFIG_SILENT_SUPPORT)
+  int instdir_error;      // GetInstDirError
+  int rtl;                // 1 if $LANGUAGE is a RTL language
+  int errlvl;             // SetErrorLevel
+  int alter_reg_view;     // SetRegView: Default View = 0, Alternative View = (sizeof(void*) > 4 ? KEY_WOW64_32KEY : KEY_WOW64_64KEY)
+  int status_update;      // SetDetailsPrint
 } exec_flags_t;
 
 #ifndef NSISCALL
