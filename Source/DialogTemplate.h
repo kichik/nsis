02511a35cb27e397006d5f133dd03fd4c6f7eebe
Anders <anders_k@users.sourceforge.net>
2018-11-01 23:09:07 +0000

    Moved POSIX dialog unit calcilation function stubs
    git-svn-id: https://svn.code.sf.net/p/nsis/code/NSIS/trunk@7036 212acab6-be3b-0410-9dea-997c60f758d6

--- Source/DialogTemplate.h
+++ Source/DialogTemplate.h
@@ -123,19 +123,17 @@ public:
 #endif
   void  MoveAll(short x, short y);
   void  Resize(short x, short y);
-#ifdef _WIN32
-  static inline bool SupportsDialogUnitComputation() { return true; }
   void  PixelsToDlgUnits(short& x, short& y);
   void  PixelsToDlgUnits(SIZE& siz);
   void  DlgUnitsToPixels(short& x, short& y);
+#ifdef _WIN32
+  static inline bool SupportsDialogUnitComputation() { return true; }
   SIZE  GetStringSize(WORD id, TCHAR *str);
   void  RTrimToString(WORD id, TCHAR *str, int margins);
   void  LTrimToString(WORD id, TCHAR *str, int margins);
   void  CTrimToString(WORD id, TCHAR *str, int margins);
 #else
   static inline bool SupportsDialogUnitComputation() { return false; }
-  inline void PixelsToDlgUnits(short& x, short& y) { assert(0); }
-  inline void DlgUnitsToPixels(short& x, short& y) { assert(0); }
 #endif
   void  ConvertToRTL();
   BYTE* Save(DWORD& dwSize);
